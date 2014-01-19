ddaa02b0cf128bd10331386e3d1a8843016f6046
homm <homm86@gmail.com>
2014-01-19 22:21:38 +0400

    compact scale

--- scale.c
+++ scale.c
@@ -104,7 +104,7 @@ Bitmap *scale_bitmap(Bitmap *bmp, int scale)
   Bitmap  *res;
   uint8_t *src1, *src2, *src3, *src4, *src5, *src6, *src7, *src8, *dst;
   size_t   srcrow, dstrow, cc;
-  size_t   x, y, c;
+  size_t   x, y;
 
   res = (Bitmap *)calloc(1, sizeof(Bitmap));
   res->width = bmp->width / scale;
@@ -189,74 +189,59 @@ Bitmap *scale_bitmap(Bitmap *bmp, int scale)
       }\
     }
 
-  #define CHANEL_COMPUTION_2X(c, cc) \
+  #define CHANEL_COMPUTION_2X(c) \
     dst[x + c] = (src1[x*2 + c] + src1[x*2 + c + cc] + \
                   src2[x*2 + c] + src2[x*2 + c + cc]) >> 2
 
-  #define CHANEL_COMPUTION_3X(c, cc) \
+  #define CHANEL_COMPUTION_3X(c) \
     dst[x + c] = (src1[x*3 + c] + src1[x*3 + c + cc] + src1[x*3 + c + cc*2] +\
                   src2[x*3 + c] + src2[x*3 + c + cc] + src2[x*3 + c + cc*2] +\
                   src3[x*3 + c] + src3[x*3 + c + cc] + src3[x*3 + c + cc*2]) / 9
 
-  #define CHANEL_COMPUTION_4X(c, cc) \
-    dst[x + c] = (src1[x*4 + c] + src1[x*4 + c + cc] + src1[x*4 + c + cc*2] + src1[x*4 + c + cc*3] +\
-                  src2[x*4 + c] + src2[x*4 + c + cc] + src2[x*4 + c + cc*2] + src2[x*4 + c + cc*3] +\
-                  src3[x*4 + c] + src3[x*4 + c + cc] + src3[x*4 + c + cc*2] + src3[x*4 + c + cc*3] +\
-                  src4[x*4 + c] + src4[x*4 + c + cc] + src4[x*4 + c + cc*2] + src4[x*4 + c + cc*3]) >> 4
+  #define SRC_COMPUTION_4X(c, src) \
+    src[x*4 + c] + src[x*4 + c + cc] + src[x*4 + c + cc*2] + src[x*4 + c + cc*3]
+  #define CHANEL_COMPUTION_4X(c) \
+    dst[x + c] = (SRC_COMPUTION_4X(c, src1) + SRC_COMPUTION_4X(c, src2) +\
+                  SRC_COMPUTION_4X(c, src3) + SRC_COMPUTION_4X(c, src4)) >> 4
 
-  #define CHANEL_COMPUTION_6X(c, cc) \
-    dst[x + c] = (src1[x*6 + c] + src1[x*6 + c + cc] + src1[x*6 + c + cc*2] + src1[x*6 + c + cc*3] +\
-                  src1[x*6 + c + cc*4] + src1[x*6 + c + cc*5] +\
-                  src2[x*6 + c] + src2[x*6 + c + cc] + src2[x*6 + c + cc*2] + src2[x*6 + c + cc*3] +\
-                  src2[x*6 + c + cc*4] + src2[x*6 + c + cc*5] +\
-                  src3[x*6 + c] + src3[x*6 + c + cc] + src3[x*6 + c + cc*2] + src3[x*6 + c + cc*3] +\
-                  src3[x*6 + c + cc*4] + src3[x*6 + c + cc*5] +\
-                  src4[x*6 + c] + src4[x*6 + c + cc] + src4[x*6 + c + cc*2] + src4[x*6 + c + cc*3] +\
-                  src4[x*6 + c + cc*4] + src4[x*6 + c + cc*5] +\
-                  src5[x*6 + c] + src5[x*6 + c + cc] + src5[x*6 + c + cc*2] + src5[x*6 + c + cc*3] +\
-                  src5[x*6 + c + cc*4] + src5[x*6 + c + cc*5] +\
-                  src6[x*6 + c] + src6[x*6 + c + cc] + src6[x*6 + c + cc*2] + src6[x*6 + c + cc*3] +\
-                  src6[x*6 + c + cc*4] + src6[x*6 + c + cc*5]) / 36
+  #define SRC_COMPUTION_6X(c, src) \
+    src[x*6 + c] + src[x*6 + c + cc] + src[x*6 + c + cc*2] +\
+    src[x*6 + c + cc*3] + src[x*6 + c + cc*4] + src[x*6 + c + cc*5]
+  #define CHANEL_COMPUTION_6X(c) \
+    dst[x + c] = (SRC_COMPUTION_6X(c, src1) + SRC_COMPUTION_6X(c, src2) +\
+                  SRC_COMPUTION_6X(c, src3) + SRC_COMPUTION_6X(c, src4) +\
+                  SRC_COMPUTION_6X(c, src5) + SRC_COMPUTION_6X(c, src6)) / 36
 
-  #define CHANEL_COMPUTION_8X(c, cc) \
-    dst[x + c] = (src1[x*8 + c] + src1[x*8 + c + cc] + src1[x*8 + c + cc*2] + src1[x*8 + c + cc*3] +\
-                  src1[x*8 + c + cc*4] + src1[x*8 + c + cc*5] + src1[x*8 + c + cc*6] + src1[x*8 + c + cc*7] +\
-                  src2[x*8 + c] + src2[x*8 + c + cc] + src2[x*8 + c + cc*2] + src2[x*8 + c + cc*3] +\
-                  src2[x*8 + c + cc*4] + src2[x*8 + c + cc*5] + src2[x*8 + c + cc*6] + src2[x*8 + c + cc*7] +\
-                  src3[x*8 + c] + src3[x*8 + c + cc] + src3[x*8 + c + cc*2] + src3[x*8 + c + cc*3] +\
-                  src3[x*8 + c + cc*4] + src3[x*8 + c + cc*5] + src3[x*8 + c + cc*6] + src3[x*8 + c + cc*7] +\
-                  src4[x*8 + c] + src4[x*8 + c + cc] + src4[x*8 + c + cc*2] + src4[x*8 + c + cc*3] +\
-                  src4[x*8 + c + cc*4] + src4[x*8 + c + cc*5] + src4[x*8 + c + cc*6] + src4[x*8 + c + cc*7] +\
-                  src5[x*8 + c] + src5[x*8 + c + cc] + src5[x*8 + c + cc*2] + src5[x*8 + c + cc*3] +\
-                  src5[x*8 + c + cc*4] + src5[x*8 + c + cc*5] + src5[x*8 + c + cc*6] + src5[x*8 + c + cc*7] +\
-                  src6[x*8 + c] + src6[x*8 + c + cc] + src6[x*8 + c + cc*2] + src6[x*8 + c + cc*3] +\
-                  src6[x*8 + c + cc*4] + src6[x*8 + c + cc*5] + src6[x*8 + c + cc*6] + src6[x*8 + c + cc*7] +\
-                  src7[x*8 + c] + src7[x*8 + c + cc] + src7[x*8 + c + cc*2] + src7[x*8 + c + cc*3] +\
-                  src7[x*8 + c + cc*4] + src7[x*8 + c + cc*5] + src7[x*8 + c + cc*6] + src7[x*8 + c + cc*7] +\
-                  src8[x*8 + c] + src8[x*8 + c + cc] + src8[x*8 + c + cc*2] + src8[x*8 + c + cc*3] +\
-                  src8[x*8 + c + cc*4] + src8[x*8 + c + cc*5] + src8[x*8 + c + cc*6] + src8[x*8 + c + cc*7]) >> 6
+  #define SRC_COMPUTION_8X(c, src) \
+    src[x*8 + c] + src[x*8 + c + cc] + src[x*8 + c + cc*2] + src[x*8 + c + cc*3] +\
+    src[x*8 + c + cc*4] + src[x*8 + c + cc*5] + src[x*8 + c + cc*6] + src[x*8 + c + cc*7]
+  #define CHANEL_COMPUTION_8X(c) \
+    dst[x + c] = (SRC_COMPUTION_8X(c, src1) + SRC_COMPUTION_8X(c, src2) +\
+                  SRC_COMPUTION_8X(c, src3) + SRC_COMPUTION_8X(c, src4) +\
+                  SRC_COMPUTION_8X(c, src5) + SRC_COMPUTION_8X(c, src6) +\
+                  SRC_COMPUTION_8X(c, src7) + SRC_COMPUTION_8X(c, src8)) >> 6
 
   #define SCALE_CASE(defPIXEL_LOOP, defCHANEL_COMPUTION) \
     switch (cc) \
     { \
       case 1: \
         defPIXEL_LOOP( \
-          defCHANEL_COMPUTION(0, cc); \
+          defCHANEL_COMPUTION(0); \
         ) \
         break; \
       case 3: \
         defPIXEL_LOOP( \
-          defCHANEL_COMPUTION(0, cc); \
-          defCHANEL_COMPUTION(1, cc); \
-          defCHANEL_COMPUTION(2, cc); \
+          defCHANEL_COMPUTION(0); \
+          defCHANEL_COMPUTION(1); \
+          defCHANEL_COMPUTION(2); \
         ) \
         break; \
       case 4: \
         defPIXEL_LOOP( \
-          defCHANEL_COMPUTION(0, cc); \
-          defCHANEL_COMPUTION(1, cc); \
-          defCHANEL_COMPUTION(2, cc); \
-          defCHANEL_COMPUTION(3, cc); \
+          defCHANEL_COMPUTION(0); \
+          defCHANEL_COMPUTION(1); \
+          defCHANEL_COMPUTION(2); \
+          defCHANEL_COMPUTION(3); \
         ) \
         break; \
     }
@@ -270,6 +255,7 @@ Bitmap *scale_bitmap(Bitmap *bmp, int scale)
     case 8: SCALE_CASE(PIXEL_LOOP_8X, CHANEL_COMPUTION_8X) break;
   }
 
+  return res;
   #undef SCALE_CASE
   #undef PIXEL_LOOP_2X
   #undef PIXEL_LOOP_3X
@@ -277,7 +263,6 @@ Bitmap *scale_bitmap(Bitmap *bmp, int scale)
   #undef CHANEL_COMPUTION_2X
   #undef CHANEL_COMPUTION_3X
   #undef CHANEL_COMPUTION_4X
-  return res;
 }
 
 
@@ -307,12 +292,12 @@ int main(int argc, char **argv)
   start = clock();
   for (i = 0; i < times; ++i)
   {
-    res = scale_bitmap(bmp, scale);
+    res = linear_resize_bitmap(bmp, scale);
     free_bitmap(res);
   }
   time = (float)(clock() - start) / CLOCKS_PER_SEC;
 
-  res = scale_bitmap(bmp, scale);
+  res = linear_resize_bitmap(bmp, scale);
   snprintf(buf, sizeof buf, "%s.scaled.bmp", argv[1]);
   save_bitmap(buf, res);
 
